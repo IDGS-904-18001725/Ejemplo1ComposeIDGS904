47194482bd3a0324846f8a509dcb473184ab3c28
IDGS-904-18001725 <xgperez97@gmail.com>
2025-06-10 19:01:05 -0600

    Tarjetas terminadas 10-06-2025

--- app/src/main/java/com/example/ejemplo1composeidgs904/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1composeidgs904/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.ejemplo1composeidgs904
 
-import android.R.attr.contentDescription
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,9 +9,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,71 +28,96 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
-import com.example.ejemplo1composeidgs904.ui.theme.Ejemplo1ComposeIDGS904Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //MessageCard("Gerardo")
             Ejemplo1ComposeIDGS904Theme {
-                Tarjeta()
-
+                ListaPersonajes()
             }
+        }
+    }
+}
+data class Personaje(val imagenResId: Int, val nombre: String,val descripcion:String)
+@Composable
+fun Tarjeta(personaje: Personaje) {
+    Card(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            ImagenHeroe(personaje.imagenResId)
+            InfoPersonaje(personaje)
+        }
+    }
+}
+@Composable
+fun ListaPersonajes() {
+    LazyColumn {
+        items(listaDragonball) { personaje ->
+            Tarjeta(personaje)
         }
     }
 }
 
 @Composable
-fun MessageCard(nombre: String) {
-    Text(text = "Hola $nombre!")
-    Text(text = "Hola $nombre! 2")
+fun MessageCard(nombre: String){
+    Text("Hola Word  $nombre")
+    Text("Hola Word 2")
 }
+private val personajes = listOf(
+    Personaje(R.drawable.goku_normal, "Goku", "El protagonista de la serie, conocido por su gran poder y personalidad amigable"),
+    Personaje(R.drawable.gohan, "Gohan", "Es el primer hijo de Son Goku y Chi-Chi"),
+    Personaje(R.drawable.Androide_17_Artwork, "Androide 17", ""),
+    Personaje(R.drawable.Androide_18_Artwork, "Androide 18", ""),
+    Personaje(R.drawable.celula, "Cell", ""),
+    Personaje(R.drawable.gogeta, "Gogeta", ""),
+    Personaje(R.drawable.Jiren, "Jiren", ""),
+    Personaje(R.drawable.Gotenks_Artwork, "Gotenks", ""),
+    Personaje(R.drawable.Daishinkan, "Daishinkan", ""),
+    Personaje(R.drawable.vegeta_normal, "Vegeta", ""),
+    Personaje(R.drawable.Vegetto, "Vegetto", "")
+)
 
-@Preview
 @Composable
-fun PreviewMessageCard() {
-    //MessageCard("Gerardo")
-    Tarjeta()
-    Tarjeta()
-    Tarjeta()
-    Tarjeta()
-    Tarjeta()
-}
-data class Personaje(val nombre: String, val descripcion: String)
-
-
-@Composable
-fun Tarjeta() {
-    Row(
-        modifier = Modifier
-            .padding(8.dp)
-            .background(MaterialTheme.colorScheme.background)
-            .fillMaxSize()
-    ){
-        ImagenHeroe()
-        Personaje()
+fun InfoPersonaje (personaje: Personaje){
+    Column(modifier = Modifier.padding(8.dp)) {
+        Text(text = "Nombre: ${personaje.nombre}", style = MaterialTheme.typography.titleMedium)
+        Text(text = "Descripción: ${personaje.descripcion}", style = MaterialTheme.typography.bodyMedium)
     }
 }
 
 @Composable
-fun ImagenHeroe(){
+fun ImagenHeroe(imagenResId: Int){
     Image(
-    painterResource(id = R.drawable.goku_normal),
-    contentDescription = "Imagen de Goku",
+        painter = painterResource(id = imagenResId),
+        contentDescription = null,
         modifier = Modifier
             .padding(16.dp)
-            .size(200.dp)
+            .size(100.dp)
             .clip(CircleShape)
-            .background(MaterialTheme.colorScheme.primary)
+            .background(MaterialTheme.colorScheme.tertiary)
     )
 }
-
+@Preview(showBackground = true)
 @Composable
-fun Personaje(){
+fun PreviewListaPersonajes() {
+
     Column {
-        Text(text = "Nombre: Goku")
-        Text(text = "Descripcion: El protagonista de la serie, conocido por su gran poder y personalidad amigable")
+        listaDragonball.forEach { personaje ->
+            Tarjeta(personaje)
+        }
     }
 }
